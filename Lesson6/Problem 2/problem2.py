8aa37690a08b880b5c30617028b454558b0331f0
samantharina <43784570+samantharina@users.noreply.github.com>
2018-12-05 12:41:46 -0500

    Add files via upload

---
+++ Lesson6/Problem 2/problem2.py
@@ -0,0 +1,17 @@
+name = input('What is your name? ')
+
+print ('Nice to meet you' + name + ', My name is Chatbot.' ) 
+
+age = input('How old are you? ') 
+
+print ('Wow' + age + ' years old! I was born today.')
+
+feeling = input('How are you feeling today? ')
+
+print ('Glad to hear you are feeling' + feeling + '. That is wonderful to hear!')
+
+color = input(
+	'Whats Your favorite color? ')
+
+print ('Me too! Well, thanks for chatting!')
+
